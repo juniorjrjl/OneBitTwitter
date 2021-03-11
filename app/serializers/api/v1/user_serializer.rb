567e69246d94224c087f357d1851e6ba29044133
Junior <junior.jr.jl@gmail.com>
2021-03-11 13:19:37 -0300

    inclusao de lista de followers e followings no serializer de user

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :description, :tweets_count, :followers_count, :following_count, :photo
+  attributes :id, :name, :email, :description, :tweets_count, :followings, :followers, :followers_count, :following_count, :photo
   attribute :followed, if: :has_user?
 
   def tweets_count
@@ -12,6 +12,14 @@ class Api::V1::UserSerializer < ActiveModel::Serializer
 
   def following_count
     object.following_users.count
+  end
+
+  def followers
+    object.followers_by_type('User')
+  end
+
+  def followings
+    object.following_users
   end
 
   def followed
